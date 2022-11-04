f16a008a0542c588a12491ff224e7ac159a18dde
Alexander Mironov <sh1yo@tuta.io>
2022-11-04 14:29:03 +0300

    Improve comments within config struct

--- src/config/structs.rs
+++ src/config/structs.rs
@@ -4,77 +4,80 @@ use crate::network::utils::DataType;
 
 #[derive(Debug, Clone)]
 pub struct Config {
-    //default urls without any changes (except from when used from request file, maybe change this logic TODO)
+    /// default urls without any changes (except from when used from request file, maybe change this logic TODO)
     pub urls: Vec<String>,
 
-    //a list of methods to check parameters with
+    /// a list of methods to check urls with
     pub methods: Vec<String>,
 
-    //custom user supplied headers or default ones
+    /// custom user supplied headers or default ones
     pub custom_headers: Vec<(String, String)>,
 
-    //how much to sleep between requests in millisecs
+    /// how much to sleep between requests in millisecs
     pub delay: Duration,
 
-    //user supplied wordlist file
+    /// user supplied wordlist file
     pub wordlist: String,
 
-    //max amount of parameters to send per request. Can be specified by user otherwise detects automatically based on request method
+    /// max amount of parameters to send per request.
+    /// Can be specified by user otherwise detects automatically based on the request method
     pub max: Option<usize>,
 
-    //parameter template, for example {k}={v}
+    /// parameter template, for example {k}={v}
     pub template: Option<String>,
 
-    //how to join parameters, for example '&'
+    /// how to join parameters, for example '&'
     pub joiner: Option<String>,
 
-    //whether to encode the query like param1=value1&param2=value2 -> param1%3dvalue1%26param2%3dvalue2
+    /// whether to encode the query like param1=value1&param2=value2 -> param1%3dvalue1%26param2%3dvalue2
     pub encode: bool,
 
-    //default body
+    /// default body
     pub body: String,
 
-    //Json type handles differently because values like null, true, ints needs to be sent without quotes
+    /// Json type handles differently because values like null, true, ints needs to be sent without quotes
+    /// Probably better to replace with just isJson for now..
     pub data_type: Option<DataType>,
 
-    //whether to include parameters like debug=true to the list
+    /// whether to include parameters like debug=true to the list
     pub disable_custom_parameters: bool,
 
-    //proxy server with schema or http:// by default.
+    /// proxy server with schema or http:// by default.
     pub proxy: String,
 
-    //file to output
+    /// file to output
     pub output_file: String,
 
-    //whether to append to the output file or overwrite
+    /// whether to append to the output file instead of overwriting
     pub append: bool,
 
-    //output format for file & stdout outputs
+    /// output format for file & stdout outputs
     pub output_format: String,
 
-    //a directory for saving request & responses with found parameters
+    /// a directory for saving request & responses with found parameters
     pub save_responses: String,
 
-    //ignore errors like 'Page is too huge'
+    /// ignore some custom errors like when page's size > MAX_PAGE_SIZE
     pub force: bool,
 
-    //only report parameteres with different "diffs"
-    //in case a few parameters change the same part of a page - only one of them will be saved
+    /// only report parameteres with different "diffs"
+    /// in case a few parameters change the same part of a page - only one of them will be saved
+    /// greatly reduces false positives and a bit increases false negatives
     pub strict: bool,
 
-    //custom parameters to check like <admin, [true, 1, false, ..]>
+    /// custom parameters to check like <admin, [true, 1, false, ..]>
     pub custom_parameters: HashMap<String, Vec<String>>,
 
-    //disable progress bar for high verbosity
     pub disable_progress_bar: bool,
 
-    //proxy to resend requests with found parameter
+    /// proxy to resend requests with found parameter
     pub replay_proxy: String,
-    //whether to resend the request once with all parameters or once per every parameter
+
+    /// whether to resend the request once with all parameters or once per every parameter
     pub replay_once: bool,
 
-    //print request & response and exit.
-    //Can be useful for checking whether the program parsed the input parameters successfully
+    /// print request & response and exit.
+    /// can be useful for checking whether the program parsed the input parameters successfully
     pub test: bool,
 
     //0 - print only critical errors and output
@@ -82,41 +85,41 @@ pub struct Config {
     //2 - print all response changes
     pub verbose: usize,
 
-    //determines how much learning requests should be made on the start
-    //doesn't include first two requests made for cookies and initial response
+    /// determines how much learning requests should be made on the start
+    /// doesn't include first two requests made for cookies and initial response
     pub learn_requests_count: usize,
 
-    //check the same list of parameters with the found parameters until there are no new parameters to be found.
-    //conflicts with --verify for now. Will be changed in the future.
+    /// checks the same list of parameters with the found parameters until there are no new parameters to be found.
+    /// conflicts with --verify for now. Will be updated in the future.
     pub recursion_depth: usize,
 
-    //amount of concurrent requests per url
+    /// amount of concurrent requests per url
     pub concurrency: usize,
 
-    //amount of concurrent url checks
+    /// amount of concurrent url checks
     pub threads: usize,
 
-    //http request timeout in seconds
+    /// http request timeout in seconds
     pub timeout: usize,
 
-    //whether the verify found parameters one time more.
-    //in future - check for _false_potives like when every parameter that starts with _ is found
+    /// whether the verify found parameters one time more.
+    /// in future wil check for _false_potives like when every parameter that starts with _ is found
     pub verify: bool,
 
-    //check only for reflected parameters in order to decrease the amount of requests
-    //usually makes 2+learn_request_count+words/max requests
-    //but in rare cases its number may be higher
+    /// check only for reflected parameters in order to decrease the amount of requests
+    /// usually makes 2+learn_request_count+words/max requests
+    /// but in rare cases its number may be higher
     pub reflected_only: bool,
 
-    //http version. 1.1 or 2
-    //TODO replace with enum
+    /// http version. 1.1 or 2
+    /// TODO replace with enum
     pub http: String,
 
-    //by default parameters are sent within the body only in case PUT or POST methods are used.
-    //it's possible to overwrite this behaviour by specifying this option
+    /// by default parameters are sent within the body only in case PUT or POST methods are used.
+    /// it's possible to overwrite this behavior by specifying this option
     pub invert: bool,
 
-    //true in case the injection points is within the header or the headers are injection point itself
+    /// true in case the injection points is within the header or the headers are injection point itself
     pub headers_discovery: bool,
 
     pub follow_redirects: bool,
